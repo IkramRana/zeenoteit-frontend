dfc4b058761f006bea37aa03040783c34ecabdf2
TaimurRafiq <taimur.mangotech@gmail.com>
2021-11-04 12:01:20 +0500

    add herokuServer Url

--- src/axios.js
+++ src/axios.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 
 export const localServer = 'http://localhost:3000/api/'; // local url
+export const herokuServer = 'https://zeenoteit.herokuapp.com/api/'; // local url
 
 const instance = axios.create({
-    baseURL: localServer,
+    baseURL: herokuServer,
 });
 
 export default instance;
