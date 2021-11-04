647b9ddd537f7735f7447ea199f4fb68fb3518c1
shaheryar1mango <shaheryar.mangotech@gmail.com>
2021-11-04 18:18:11 +0500

    form validations

--- src/utils/index.js
+++ src/utils/index.js
@@ -23,4 +23,4 @@ export const disabledInspect = (time) => {
 }
 
 // *Email Regex
-export const emailRegex = '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[a-z]{2,3}$'
+export const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
