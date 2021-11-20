858525661ef2114f384fd3632979d4f9de14e83e
shaheryar1mango <shaheryar.mangotech@gmail.com>
2021-11-20 11:14:34 +0500

    some changes

--- src/pages/missions/my-missions.js
+++ src/pages/missions/my-missions.js
@@ -5,8 +5,6 @@ import { disabledInspect } from 'utils/index';
 import { Service } from "config/service";
 
 import { Breadcrumbs, CardHeader, Grid, IconButton, Menu, Typography } from '@material-ui/core';
-import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
-import { GridContextProvider, GridDropZone, GridItem, swap, move } from "react-grid-dnd";
 import { ToastContainer, toast } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -17,8 +15,6 @@ import AddTask from "components/add-task";
 import AddSubTask from "components/add-subtask";
 import EditTaskList from "components/edit-task-list";
 import Deleted from "components/delete";
-import DraggableElement from "./draggableElement";
-import SubTaskElement from "./subtaskelement";
 
 var columnNo = '';
 var taskId = '';
@@ -270,76 +266,6 @@ function MyMissions() {
     }
   }
 
-
-
-
-
-  const getItems = (count, prefix) =>
-    Array.from({ length: count }, (v, k) => k).map((k) => {
-      const randomId = Math.floor(Math.random() * 1000);
-      return {
-        id: `item-${randomId}`,
-        prefix,
-        content: `Task ${randomId}`
-      };
-    });
-
-
-  const removeFromList = (list, index) => {
-    const result = Array.from(list);
-    const [removed] = result.splice(index, 1);
-    return [removed, result];
-  };
-
-  const addToList = (list, index, element) => {
-    const result = Array.from(list);
-    result.splice(index, 0, element);
-    return result;
-  };
-
-
-
-  const lists = ["column1", "column2", "column3", "column4", "column5"];
-
-  const generateLists = () =>
-    lists.reduce(
-      (acc, listKey) => ({ ...acc, [listKey]: getItems(2, listKey) }),
-      {}
-    );
-
-
-  const [elements, setElements] = React.useState(generateLists());
-
-  useEffect(() => {
-    setElements(generateLists());
-  }, []);
-
-  const onDragEnd = (result) => {
-    if (!result.destination) {
-      return;
-    }
-    const listCopy = { ...elements };
-
-    const sourceList = listCopy[result.source.droppableId];
-    const [removedElement, newSourceList] = removeFromList(
-      sourceList,
-      result.source.index
-    );
-    listCopy[result.source.droppableId] = newSourceList;
-    const destinationList = listCopy[result.destination.droppableId];
-    listCopy[result.destination.droppableId] = addToList(
-      destinationList,
-      result.destination.index,
-      removedElement
-    );
-
-    setElements(listCopy);
-  };
-
-
-
-
-
   useEffect(() => {
     getTask();
     getColors();
@@ -400,182 +326,82 @@ function MyMissions() {
           {/* ========== Missions ========== */}
           <Grid className="mission" container spacing={0} justifyContent="flex-start" alignItems="flex-start">
 
-            {/* <DragDropContext onDragEnd={onDragEnd}>
-              {[...Array(5)].map((x, i) => (
-                <Grid key={i} className="wrapper" container spacing={0} item md={2}>
-                  <Droppable droppableId="droppable">
-                    {(provided) => (
-                      <div
-                        style={{ width: '100%', minHeight: '10px' }}
-                        ref={provided.innerRef}
-                        {...provided.droppableProps}
-                      >
-                        {task.map((task, index) => {
-                          if ((i + 1) === task.column_no) {
-                            return (
-                              <Draggable
-                                key={task._id}
-                                draggableId={task._id}
-                                index={index}
+            {[...Array(5)].map((x, i) => (
+              <Grid key={i} className="wrapper" container spacing={0} item md={2}>
+                {task.map((task, index) => {
+                  if ((i + 1) === task.column_no) {
+                    return (
+                      <Grid key={task._id} ref={provided.innerRef}
+                        {...provided.draggableProps} className="task-box" item md={12} style={{ borderColor: task.color[0].code + 'a6' }}>
+                        <div className="header"  {...provided.dragHandleProps} style={{ backgroundColor: task.color[0].code + '1a' }} >
+                          <Grid container spacing={0} justifyContent="space-between" alignItems="center">
+                            <Grid item md={8}>
+                              <Typography component="h5">{task.title}</Typography>
+                            </Grid>
+                            <Grid item md={2}>
+                              <IconButton aria-label="menu" size="small" onClick={() => { subTaskDialog(true, task._id) }}>
+                                <Plus />
+                              </IconButton>
+                            </Grid>
+                            <Grid item md={2}>
+                              <IconButton aria-label="menu" size="small" onClick={(e) => { menuHandler(index, e) }}>
+                                {anchorEl && Boolean(anchorEl[index]) === true ? <VerticalMenu /> : <More />}
+                              </IconButton>
+
+                              {/* ========== Menu Options ========== */}
+                              <Menu
+                                className="menu-option"
+                                anchorEl={anchorEl && anchorEl[index]}
+                                keepMounted
+                                open={anchorEl && Boolean(anchorEl[index])}
+                                onClose={handleClose}
+                                getContentAnchorEl={null}
+                                anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+                                transformOrigin={{ vertical: "top", horizontal: "center" }}
                               >
-                                {(provided) => {
-                                  return (
-                                    <Grid key={task._id} ref={provided.innerRef}
-                                      {...provided.draggableProps} className="task-box" item md={12} style={{ borderColor: task.color[0].code + 'a6' }}>
-                                      <div className="header"  {...provided.dragHandleProps} style={{ backgroundColor: task.color[0].code + '1a' }} >
-                                        <Grid container spacing={0} justifyContent="space-between" alignItems="center">
-                                          <Grid item md={8}>
-                                            <Typography component="h5">{task.title}</Typography>
-                                          </Grid>
-                                          <Grid item md={2}>
-                                            <IconButton aria-label="menu" size="small" onClick={() => { subTaskDialog(true, task._id) }}>
-                                              <Plus />
-                                            </IconButton>
-                                          </Grid>
-                                          <Grid item md={2}>
-                                            <IconButton aria-label="menu" size="small" onClick={(e) => { menuHandler(index, e) }}>
-                                              {anchorEl && Boolean(anchorEl[index]) === true ? <VerticalMenu /> : <More />}
-                                            </IconButton> */}
+                                <IconButton className="edit" aria-label="edit" onClick={() => { editTaskDialog(true, task._id, task.title) }}>
+                                  <EditTask />
+                                </IconButton>
+                                <IconButton className="deleted" aria-label="deleted" onClick={() => { deleteTaskDialog(true, task._id) }}>
+                                  <Trash />
+                                </IconButton>
+                              </Menu>
+                            </Grid>
+                          </Grid>
+                        </div>
+                        <div className="content">
+                          {task.subtasks.map((subTask, i) => (
+                            <div key={i} className="task">
+                              <div className="checkbox">
+                                {subTask.isCompleted &&
+                                  <input type="checkbox" checked={true} id={subTask._id} />
+                                }
+                                {subTask.isCompleted === false &&
+                                  <input type="checkbox" id={subTask._id} onClick={(e) => taskComplete(subTask._id)} />
+                                }
+                                <label for={subTask._id}></label>
+                              </div>
+                              <Typography className={subTask.isCompleted == true ? 'text-strike' : ''} component="p">{subTask.title}</Typography>
+                            </div>
+                          ))}
 
-            {/* ========== Menu Options ========== */}
-            {/* <Menu
-                                              className="menu-option"
-                                              anchorEl={anchorEl && anchorEl[index]}
-                                              keepMounted
-                                              open={anchorEl && Boolean(anchorEl[index])}
-                                              onClose={handleClose}
-                                              getContentAnchorEl={null}
-                                              anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
-                                              transformOrigin={{ vertical: "top", horizontal: "center" }}
-                                            >
-                                              <IconButton className="edit" aria-label="edit" onClick={() => { editTaskDialog(true, task._id, task.title) }}>
-                                                <EditTask />
-                                              </IconButton>
-                                              <IconButton className="deleted" aria-label="deleted" onClick={() => { deleteTaskDialog(true, task._id) }}>
-                                                <Trash />
-                                              </IconButton>
-                                            </Menu>
-                                          </Grid>
-                                        </Grid>
-                                      </div>
-                                      <SubTaskElement /> */}
-            {/* <div className="content">
-                                        {task.subtasks.map((subTask, i) => (
-                                          <div key={i} className="task">
-                                            <div className="checkbox">
-                                              {subTask.isCompleted &&
-                                                <input type="checkbox" checked={true} id={subTask._id} />
-                                              }
-                                              {subTask.isCompleted === false &&
-                                                <input type="checkbox" id={subTask._id} onClick={(e) => taskComplete(subTask._id)} />
-                                              }
-                                              <label for={subTask._id}></label>
-                                            </div>
-                                            <Typography className={subTask.isCompleted == true ? 'text-strike' : ''} component="p">{subTask.title}</Typography>
-                                          </div>
-                                        ))}
+                          <div className="add-subtask cursor-pointer" onClick={() => { subTaskDialog(true, task._id) }}>
+                            <Plus />
+                            <Typography component="p">Add New Task</Typography>
+                          </div>
+                        </div>
+                      </Grid>
+                    )
+                  }
+                })}
 
-                                        <div className="add-subtask cursor-pointer" onClick={() => { subTaskDialog(true, task._id) }}>
-                                          <Plus />
-                                          <Typography component="p">Add New Task</Typography>
-                                        </div>
-                                      </div> */}
-            {/* </Grid>
-                                  );
-                                }}
-                              </Draggable>
-                            )
-                          }
-                        })}
-                        {provided.placeholder}
-                      </div>
-                    )}
-                  </Droppable>
-
-                  <Grid className="add-task" item md={12} onClick={() => { taskDialog(true, (i + 1)) }}>
-                    <Plus />
-                    <Typography component="span">Add To Do List</Typography>
-                  </Grid>
-
-                </Grid>
-              ))}
-            </DragDropContext> */}
-
-            {/* <DragDropContext onDragEnd={onDragEnd}>
-              {lists.map((listKey) => (
-                < DraggableElement
-                  elements={elements[listKey]}
-                  key={listKey}
-                  prefix={listKey}
-                />
-              ))}
-            </DragDropContext> */}
-
-
-            {/* <DragDropContext onDragEnd={onDragEnd}>
-              {lists.map((listKey) => (
-                // < DraggableElement
-                //   elements={elements[listKey]}
-                //   key={listKey}
-                //   prefix={listKey}
-                // />
-
-                <Grid key={listKey} className="wrapper" container spacing={0} item md={2}>
-                  <Droppable droppableId={listKey}>
-                    {(provided) => (
-                      <div
-                        style={{ width: '100%', minHeight: '10px' }}
-                        ref={provided.innerRef}
-                        {...provided.droppableProps}
-                      >
-                        {elements[listKey].map((item, index) => (
-                          // <ListItem key={item.id} item={item} index={index} />
-                          <Draggable
-                            key={item.id}
-                            draggableId={item.id}
-                            index={index}
-                          >
-                            {(provided) => {
-                              return (
-                                <Grid className="task-box" item md={12}
-                                  ref={provided.innerRef}
-                                  {...provided.draggableProps}
-                                >
-                                  <div className="header"  {...provided.dragHandleProps}>
-                                    <Grid container spacing={0} justifyContent="space-between" alignItems="center">
-                                      <Grid item md={8}>
-                                        <Typography component="h5">{item.content}</Typography>
-                                      </Grid>
-                                      <Grid item md={2}>
-                                        <IconButton aria-label="menu" size="small">
-                                          <Plus />
-                                        </IconButton>
-                                      </Grid>
-                                      <Grid item md={2}>
-                                        <IconButton aria-label="menu" size="small">
-                                          <More />
-                                        </IconButton>
-                                      </Grid>
-                                    </Grid>
-                                  </div>
-                                  <SubTaskElement />
-                                </Grid>
-                              );
-                            }}
-                          </Draggable>
-                        ))}
-                        {provided.placeholder}
-                      </div>
-                    )}
-                  </Droppable>
-                  <Grid className="add-task" item md={12} >
-                    <Plus />
-                    <Typography component="span">Add To Do List</Typography>
-                  </Grid>
+                <Grid className="add-task" item md={12} onClick={() => { taskDialog(true, (i + 1)) }}>
+                  <Plus />
+                  <Typography component="span">Add To Do List</Typography>
                 </Grid>
 
-              ))}
-            </DragDropContext> */}
+              </Grid>
+            ))}
 
 
           </Grid>
