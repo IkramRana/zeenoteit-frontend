a895433677fa63495e559f9864e3cf09b0df2e42
TaimurRafiq <rafiqtaimur@gmail.com>
2022-06-29 08:26:51 +0500

    fix url bug

--- src/axios.js
+++ src/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 export const localServer = 'http://localhost:3000/api/'; // Local URL
-export const herokuServer = 'https://zeenoteit.herokuapp.com/api/'; // Heroku Live URL
-export const liveServer = 'http://app.zeenoteit.com/api/'; // Live URL
+export const herokuServer = 'https://zenoteit.herokuapp.com/api/'; // Heroku Live URL
+export const liveServer = 'http://app.zenoteit.com/api/'; // Live URL
 
 const instance = axios.create({
     baseURL: liveServer,
--- src/config/socket.js
+++ src/config/socket.js
@@ -3,7 +3,7 @@ import io from 'socket.io-client';
 export const socketConfig = () => {
 
     //const socket = io("ws://localhost:3018",
-    const socket = io("wss://app.zeenoteit.com/notification-service:3018",
+    const socket = io("wss://app.zenoteit.com/notification-service:3018",
     {
         rejectUnauthorized: false,
         reconnection: true
