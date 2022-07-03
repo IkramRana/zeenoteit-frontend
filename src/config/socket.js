c7d193fc5911fd1645cab24ef7f227318f2199e9
TaimurRafiq <rafiqtaimur@gmail.com>
2022-07-03 13:15:21 +0500

    test

--- src/config/socket.js
+++ src/config/socket.js
@@ -2,7 +2,7 @@ import io from 'socket.io-client';
 
 export const socketConfig = () => {
 
-    //const socket = io("ws://localhost:3018",
+    //const socket = io("ws://localhost:3018", 
     const socket = io("ws://app.zenoteit.com/notification-service",
     {
         rejectUnauthorized: false,
