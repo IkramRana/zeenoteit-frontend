9e7dfbecccfc98337157816dcff3bb852c803748
ShairyarMangotech <shairyar.mangotech@gmail.com>
2023-10-09 15:37:52 +0500

    check platform

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import AuthProvider from "hoc/authProvider";
 import AppRoutes from 'routes';
 
 function App() {
+  const [platform, setPlatform] = useState('');
+  console.log("🚀 ~ file: App.js:7 ~ App ~ platform:", platform)
+  useEffect(() => {
+    const userAgent = navigator.userAgent;
+
+    if (userAgent.match(/Windows/i)) {
+      setPlatform('Windows');
+    } else if (userAgent.match(/Macintosh/i)) {
+      setPlatform('macOS');
+    } else if (userAgent.match(/Linux/i)) {
+      setPlatform('Linux');
+    } else if (userAgent.match(/iPhone|iPad|iPod/i)) {
+      setPlatform('iOS');
+    } else if (userAgent.match(/Android/i)) {
+      setPlatform('Android');
+    } else {
+      setPlatform('Unknown');
+    }
+  }, []);
 
   return (
     <AuthProvider>
